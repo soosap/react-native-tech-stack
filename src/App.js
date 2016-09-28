89117d683a687dd4ca3b0e3082e4f77e7be6e08a
soosap <prasath.soosaithasan@gmail.com>
2016-09-28 12:17:36 +0200

    Provide flex:1 style prop to <App /> to propagate entire available space

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
@@ -7,15 +7,13 @@ import { Header } from './components/common';
 
 import LibraryList from './components/LibraryList';
 
-const App = () => {
-	return (
-		<Provider store={createStore(reducers)}>
-			<View>
-				<Header title="TechStack" />
-				<LibraryList />
-			</View>
-		</Provider>
-	);
-};
+const App = () => (
+	<Provider store={createStore(reducers)}>
+		<View style={{ flex: 1 }}>
+			<Header title="TechStack" />
+			<LibraryList />
+		</View>
+	</Provider>
+);
 
 export default App;
