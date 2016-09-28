2a3285842f0412004913913c7fb4917edb0f6bab
soosap <prasath.soosaithasan@gmail.com>
2016-09-28 10:14:20 +0200

    Add <Header /> component to <App />

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-
 import reducers from './reducers';
+import { Header } from './components/common';
 
 const App = () => {
 	return (
 		<Provider store={createStore(reducers)}>
 			<View>
+				<Header title="TechStack" />
 				<Text>App</Text>
 			</View>
 		</Provider>
